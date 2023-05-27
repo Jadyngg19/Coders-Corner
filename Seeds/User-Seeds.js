8cc8050ecab1fbc98f76e375678507bd71f88c4c
Jadyn Gomez <jadyngg19@gmail.com>
2023-05-27 11:50:19 -0400

    Adding in user seed

---
+++ Seeds/User-Seeds.js
@@ -0,0 +1,25 @@
+const {User} = require('../Models');
+const UserData = [
+    {
+        username: "Jadyngg_19",
+        github: "Jadyngg19",
+        email: "jadyngg19@gmail.com",
+        password: "ROYGBP2023$"
+    },
+
+    {
+        username: "Alex_Gaskarth",
+        github: "cupokoffi8",
+        email: "alexg@gmail.com",
+        password: "Monstertruck88!"
+    },
+
+    {
+        username: "WinnyG",
+        github: "Winny#01",
+        email: "winny01@gmail.com",
+        password: "HoseNose45*"
+    }
+]
+
+const seedUsers = () => User.bulkCreate(userData);
